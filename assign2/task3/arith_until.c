853fe64005e6cb8c60aaefea4f8291bb4e519bd3
allison <55728999+am-f@users.noreply.github.com>
2021-02-13 01:47:40 -0500

    Add files via upload

--- assign2/task3/arith_until.c
+++ assign2/task3/arith_until.c
@@ -3,33 +3,27 @@
 #include <stdio.h>
 #define PI 3.14159
 
-void calculation(float radiusMil) {
-  float radiusIn, areaIn, circIn;
-  if (radiusMil == 0) {
-    printf("Exit\n");
-    return;
-  }
-  else {
-    radiusIn = radiusMil / 25.4;
-    areaIn = PI * radiusIn * radiusIn;
-    circIn = PI * 2 * radiusIn;
-    printf("Circle's area is %3.2f (sq in).\n", areaIn);
-  printf("Its circumference is %3.2f (in).\n", circIn);
-  printf("Enter radius (in mm):\n");
-  scanf("%f", &radiusMil);
-  calculation(radiusMil);
-  return;
-
-  }
-
-}
 
 
 int main()
 {
-  float r;
+
+  int exit = 0;
+  float radiusMil, radiusIn, areaIn, circIn;
   printf("Enter radius (in mm):\n");
-  scanf("%f", &r);
-  calculation(r);
-  
+  scanf("%f", &radiusMil);
+  while (exit == 0) {
+    if(radiusMil == 0) {
+      printf("Exit\n");
+      return 0;
+    }
+    radiusIn = radiusMil / 25.4;
+    areaIn = PI * radiusIn * radiusIn;
+    circIn = PI * 2 * radiusIn;
+    printf("Circle's area is %3.2f (sq in).\n", areaIn);
+    printf("Its circumference is %3.2f (in).\n", circIn);
+    printf("Enter radius (in mm):\n");
+    scanf("%f", &radiusMil);
+  }
+  return 0;
 }
