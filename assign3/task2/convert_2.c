ec47ba3a7ab6ee69d4c31c2d31c3d55472d9e4b1
Allison Fisher <af80647n@pace.edu>
2021-02-19 07:07:58 +0000

    finished task2

--- assign3/task2/convert_2.c
+++ assign3/task2/convert_2.c
@@ -7,12 +7,36 @@
 char* convert_2(int dec)
 {
 	char *binString= (char *)malloc(42 * sizeof(char));
-  char *rawBin = (char *) malloc(32 * sizeof(char));
+  //char *rawBin = (char *) malloc(32 * sizeof(char));
+  char rawBinArr[42];
   int test = dec >> 1;
-  
+  int workingBin = dec;
+  for (int i = 31; i >= 0; i--) {
+    if(workingBin & 1) {
+      rawBinArr[i] = '1';
+    }
+    else {
+      rawBinArr[i] = '0';
+    }
+    workingBin = workingBin >> 1;
+  }
+  binString[0] = '0';
+  binString[1] = 'b';
+
+  for(int j = 6; j < 42; j+=5) {
+    binString[j] = ' ';
+  }
+  int i = 0;
+  while(i < 32) {
+    for(int j = 2; j < 42; j++) {
+      if(binString[j] == NULL) {
+        binString[j] = rawBinArr[i];
+        i++;
+      }
+    }
+  }
 
   return binString;
-
 }
 
 int main() {
@@ -23,4 +47,5 @@ int main() {
 	bin = convert_2(n);
 	printf("The Binary Notation of %d is\t %s\n", n, bin);
 	//TODO: do we need to release the memory of bin?
+  free(bin);
 }
