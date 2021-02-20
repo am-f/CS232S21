cfad1975ab63e9833bfd0ec4615f1e8245118a7a
Allison Fisher <af80647n@pace.edu>
2021-02-20 03:42:30 +0000

    update task1

--- assign3/task1/midpoint.c
+++ assign3/task1/midpoint.c
@@ -4,6 +4,9 @@
 int midpoint_original(int x, int y) {
     return (x + y)/2;
 }
+int midpoint_A(int x, int y) {
+  return x + ((y - x) / 2);
+}
 
 int midpoint_B(int x, int y) {
   unsigned int uX = (unsigned int)x;
@@ -22,13 +25,24 @@ int midpoint_C(int x, int y) {
   int xorshift = xor >> 1;
     return (x & y) + ((x ^ y) >> 1);
 }
+int midpoint_O(int x, int y) {
+  int and = x & y;
+  int xor = x ^ y;
+  int xorshift = xor >> 1;
+  return (x & y) | ((x ^ y) >> 1);
+}
 
 int main(int argc, char ** argv) {
     int a = atoi(argv[1]); //atoi converts string to int
     int b = atoi(argv[2]);
-    printf("midpoint_original = %d\n", midpoint_original(a, b));
+    //printf("midpoint_original = %d\n", midpoint_original(a, b));
 
-    //printf("midpoint_B = %d\n", midpoint_B(a, b));    
+    printf("input x: %d\n", a);
+    printf("input y: %d\n", b);
+    printf("midpoint_A = %d\n", midpoint_A(a, b)); 
+    printf("midpoint_B = %d\n", midpoint_B(a, b)); 
+
     printf("midpoint_C = %d\n", midpoint_C(a, b));
+    //printf("midpoint_O = %d\n", midpoint_O(a, b));
     return 0;
 }
