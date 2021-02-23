23130865b17a5d2085884401e7fc4b814447e58a
Allison Fisher <af80647n@pace.edu>
2021-02-23 22:06:46 +0000

    task3 done

--- assign4/task3/wordstats.c
+++ assign4/task3/wordstats.c
@@ -6,12 +6,25 @@ int main () {
 
   /* Zero out the array */
   int letter_frequency[26] = {0};
-  int len, i;
+  int len = 0;
+  int i;
   char buf[MAX_BUF];
+  int end = 0;
 
-  do {
-  } while (len > 1);
+  while(end == 0) {
+    fgets((buf + len), MAX_BUF, stdin);
+    if(buf[len] == '\n') {
+      end = 1;
+    }
+  }
 
+  for(i = 0; i < strlen(buf); i++) {
+    if(buf[i] > 96 && buf[i] < 123) {
+      buf[i] = buf[i] - 32;
+     }
+    letter_frequency[(int)buf[i] - 65]++;
+  }
+  
   printf("Distribution of letters in corpus:\n");
   for (i = 0; i < 26; i++) {
     // 'A' is ASCII code 65
