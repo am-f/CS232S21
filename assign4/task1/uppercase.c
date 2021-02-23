edb6c7d70e6d0ca0fee8f69ad60a68e06d92f2ad
repl.it user <replituser@example.com>
2021-02-23 05:44:48 +0000

    assign4 task1 complete (needs format editing)

--- assign4/task1/uppercase.c
+++ assign4/task1/uppercase.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-/* example:
- *  char str[10090];
-   int ch, n = 0;
 
-   while ((ch = getchar()) != EOF && n < 1000) {
-      str[n] = ch;
+  char str[1000];
+  int ch, n = 0;
+
+   while ((ch = getchar()) != EOF /*&& n < 1000*/) {
+     if(ch == '\n') {
+       break;
+     }
+     else if(ch > 96 && ch < 123) {
+        str[n] = ch - 32;
+     }
+      else {
+        str[n] = ch;
+      }
+
       ++n;
    }
 
-   for (int i = 0; i < n; ++i)
+   for (int i = 0; i < n; ++i) {
       putchar(str[i]);
-
-
-   return 0;
-
- */
+   }
+   printf("\n");
 
     return 0;
 }
