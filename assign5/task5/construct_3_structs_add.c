ec1702895fcc73565c424d41f230ead46ada0193
allison <af80647n@pace.edu>
2021-03-14 18:58:05 +0000

    edit assign 5 task 5

--- assign5/task5/construct_3_structs_add.c
+++ assign5/task5/construct_3_structs_add.c
@@ -4,17 +4,64 @@
 typedef struct snode node_t;
 
 node_t * setup() {
-    //TODO:copy setup func from task1
+    node_t * head;
+    node_t * node1 = (node_t *)malloc(sizeof(node_t));
+    node_t * node2 = (node_t *)malloc(sizeof(node_t));
+    node_t * node3 = (node_t *)malloc(sizeof(node_t));
+    head = node1;
+
+    char str1[] = "hello";
+    int i = 0;
+    while(str1[i] != '\0') {
+      *(node1->str+i) = str1[i];
+      i++;
+    }
+    *(node1->str+i) = '\0';
+    node1->length = 5;
+    node1->next = node2;
+
+    char str2[] = "there";
+    int j = 0;
+    while(str2[j] != '\0') {
+      *(node2->str+j) = str2[j];
+      j++;
+    }
+    *(node2->str+j) = '\0';
+    node2->length = 5;
+    node2->next = node3;
+
+    char str3[] = "prof";
+    int k = 0;
+    while(str1[k] != '\0') {
+      *(node3->str+k) = str3[k];
+      k++;
+    }
+    *(node3->str+k) = '\0';
+    node3->length = 4;
+    node3->next = NULL;
+    
+   return head;
 }
 
-void teardown(/*what parameter?*/) {
-    //TODO: free all dynamic memory you requested.
-    //Please complete the prototype of teardown.
-    //You are not allowed to use globals
+void teardown(node_t * head) {
+  free(head->next->next->next);
+  free(head->next->next);
+  free(head->next);
+  free(head);
 }
 
 void add(node_t ** head, char * str, int length){
-    //TODO: implement add to add a new node to front, pointed by head
+  node_t * temp = *head;
+  *head = (node_t *)malloc(sizeof(node_t));
+  int i = 0;
+  while(*(str + i) != '\0') {
+    (*head)->str[i] = *(str + i);
+    i++;
+  }
+  (*head)->str[i] = '\0';
+  (*head)->length = length;
+  (*head)->next = temp;
+
 }
 
 void dump_all(node_t*);
@@ -22,7 +69,7 @@ int main (int argc, char ** argv) {
     node_t * head = setup();
     add(&head, "hi", 2);
     dump_all(head);
-    teardown(/*what argument?*/);
+    teardown(head);
     return 0;
 }
 
