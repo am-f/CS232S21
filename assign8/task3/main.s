6fab0ed04825a5647060f6d2f31c9a1679208408
Allison Fisher <af80647n@pace.edu>
2021-04-23 23:54:27 -0400

    assign8 task3 complete

--- assign8/task3/main.s
+++ assign8/task3/main.s
@@ -5,17 +5,17 @@ main:
   movl $13,-4(%ebp) //put a=13 into stack
   movl $5,-8(%ebp) //put b=5 into stack
   movl $0,-12(%ebp) //put c=0 into stack
-  cmpl $6,-4(%ebp)
-  jle .L2
-  movl $15,-8(%ebp)
+  cmpl $6,-4(%ebp) //compare a=13 and 6
+  jle .L2 // a <= 6 is false, so continue to if
+  movl $15,-8(%ebp) //inside if a > 6, put b=15 into stack
 .L2:
 .L3:
-  cmpl $14,-12(%ebp)
-  jg .L4
-  movl -12(%ebp),%eax
-  addl %eax,-4(%ebp)
-  incl -12(%ebp)
-  jmp .L3
+  cmpl $14,-12(%ebp) //compare c=0 and 14, do 0-14
+  jg .L4 //-14 < 0 so don't jump
+  movl -12(%ebp),%eax //put c=0 into reg %eax
+  addl %eax,-4(%ebp) //add %eax+13 and put it in a
+  incl -12(%ebp) //increment c
+  jmp .L3 //back to beginning of loop
 .L4:
   leave
   ret
